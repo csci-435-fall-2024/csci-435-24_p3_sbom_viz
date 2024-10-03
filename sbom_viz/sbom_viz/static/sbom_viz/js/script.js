a391866a7f50e46a52e0b60abccb306d8cc6a0b9
sdwalker <sdwalker@wm.edu>
2024-10-03 10:17:45 -0400

    Application can now select and upload files and log them to the console

--- sbom_viz/sbom_viz/static/sbom_viz/js/script.js
+++ sbom_viz/sbom_viz/static/sbom_viz/js/script.js
@@ -5,9 +5,11 @@ class App {
     // Set up the web application
     constructor() {
         this.page = new FileInputPage(this);  // current "page" of the application (the state of what part of the application is rendered to the screen)
+        this.fileText = ""; // temporary, will later be stored in a database
         this.appElement = document.getElementById("app"); // corresponding HTML element to modify view
         this.render(); // Set up the view
     }
+
     // Update the view by rendering the current page
     render() {
         this.page.render();
@@ -23,12 +25,53 @@ class FileInputPage {
     // Set up the page
     constructor(app) {
         this.app = app;
+        this.file = null;
+        this.selectFile = this.selectFile.bind(this);
+        this.uploadFileText = this.uploadFileText.bind(this);
     }
+
     // Render a form asking the user to input a file.
     render() {
         this.app.appElement.innerHTML = `
-            <h1>Hello, OOP!</h1>
+            <form>
+                <!-- Radio Selector for SBOM Standard -->
+                <label for="file-standard-selector">Select SBOM Standard:</label>
+                <div id="file-standard-selector">
+                    <!-- SPDX Radio Option -->
+                    <label for="file-standard-spdx">SPDX</label>
+                    <input type="radio" name="file-standard-selector-option" id="file-standard-spdx" value="spdx" checked />
+                    <!-- CycloneDX Radio Option -->
+                    <label for="file-standard-cyclone-dx">CycloneDX</label>
+                    <input type="radio" name="file-standard-selector-option" id="file-standard-cyclone-dx" value="cyclone-dx" />
+                </div>
+                <div>
+                    <!-- SBOM File Select Button -->
+                    <label for="file-select-input">Select File:</label>
+                    <input type="file" name="file-select-input" id="file-select-input"/>
+                </div>
+                <div>
+                    <!-- SBOM Upload File Button -->
+                    <button type="button" id="upload-button" disabled>Upload File</button>
+                </div>
+            </form>
         `; 
+        document.getElementById("file-select-input").addEventListener("change", this.selectFile);
+        document.getElementById("upload-button").addEventListener("click", this.uploadFileText);
+    }
+
+    // Store the file name
+    selectFile(event) {
+        this.file = event.target.files[0];
+        document.getElementById("upload-button").disabled = (this.file ? false : true);
+    }
+
+    // Read a file when uploaded
+    uploadFileText() {
+        let fileReader = new FileReader();
+        fileReader.onload = () => {
+            console.log(fileReader.result);
+        };
+        fileReader.readAsText(this.file);
     }
 }
 
