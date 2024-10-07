61de139c39401b4b376077b58ddc8e1859a8ba0f
tgeby <tgeby@icloud.com>
2024-10-07 11:25:55 -0400

    After a file is submitted to the backend, now it is read into a string and passed to the display file page. (Issue #3)

--- sbom_viz/sbom_viz/views.py
+++ sbom_viz/sbom_viz/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 def home(request):
     if request.method == "POST" and len(request.FILES) == 1:
-        return render(request, 'sbom_viz/display_file.html')
+        file = request.FILES["file-select-input"]
+        file_contents = ""
+        for line in file:
+            file_contents += line.decode()+'\n'
+        return render(request, 'sbom_viz/display_file.html', {"file_contents": file_contents})
     else:
         return render(request, 'sbom_viz/index.html')
