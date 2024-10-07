6a71e2b8286ffa736c89c151741511091c32a206
tgeby <tgeby@icloud.com>
2024-10-07 11:20:15 -0400

    Linked a new html template to be used to display the file contents after a file is submitted. (Issue #3)

--- sbom_viz/sbom_viz/views.py
+++ sbom_viz/sbom_viz/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    if request.method == "POST":
-        return HttpResponse("Form posted")
+    if request.method == "POST" and len(request.FILES) == 1:
+        return render(request, 'sbom_viz/display_file.html')
     else:
         return render(request, 'sbom_viz/index.html')
