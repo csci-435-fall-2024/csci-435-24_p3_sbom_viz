2816c334dc8eb6e75bb9e8da574549cf67aa5c0c
tgeby <tgeby@icloud.com>
2024-10-07 11:11:03 -0400

    Pulled out the file submission form from JS to a Django html template to allow for POSTing the file with csrf_token (Issue #3)

--- sbom_viz/sbom_viz/views.py
+++ sbom_viz/sbom_viz/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'sbom_viz/index.html')
+    if request.method == "POST":
+        return HttpResponse("Form posted")
+    else:
+        return render(request, 'sbom_viz/index.html')
