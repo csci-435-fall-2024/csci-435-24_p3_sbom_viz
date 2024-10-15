e4e8664222b4d312ae5ff289a631b97c05094436
sdwalker <sdwalker@wm.edu>
2024-10-15 10:44:33 -0400

    Set up D3.js and added small test rectangle

--- Code/sbom_viz/sbom_viz/static/sbom_viz/js/fileDisplayPage.js
+++ Code/sbom_viz/sbom_viz/static/sbom_viz/js/fileDisplayPage.js
@@ -1,23 +1,57 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+const tempTree = {
+    name: "1",
+    children: [
+        {name: "2", children: [
+            {name: "4"},
+            {name: "5"},
+        ]},
+        {name: "3"},
+        {name: "6", children: [
+            {name: "7", children: [
+                {name: "8"}
+            ]}
+        ]}
+    ]
+};
+
 /**
  * Represents the page that displays the contents of the file after it has been
  * loaded.
  */
 export class FileDisplayPage {
     // Set up the page
-    constructor(app, text) {
-        this.app = app;
-        this.text = text; // text contents of the file
+    constructor() {
+        console.log("Hello")
+        this.updateTree();
     }
+    updateTree() {
+        /*// Figure dimensions
 
-    // Render the page to display the file
-    render() {
-        this.app.appElement.innerHTML = `
-            <h1>Display File</h1>
-            <p id="text-p"></p>
-        `;
-        /* Set the text of the paragraph to display the text of the file.
-         * NOTE: Do NOT use innerHTML here, because it will interpret the elements of an XML document
-         * as HTML elements rather than plaintext, and an XML document may not be correctly displayed! */
-        document.getElementById("text-p").textContent = this.text;
+        const root = d3.hierarchy(tempTree);
+        const dx = 20;
+        const dy = 20;
+        
+        const tree = d3.layout.tree().nodeSize([dx, dy]);
+        const diagonal = d3.svg.diagonal().projection((d) => [d.x + dx/2, d.y + dy/2]);
+        */
+
+        // Create SVG
+        const svg = d3.select("body")
+                      .append("svg")
+                      .attr("width", 100)
+                      .attr("height", 100);
+        
+        const rect = d3.select("svg")
+          .append("rect")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("width", 10)
+          .attr("height", 10)
+          .attr("stroke", "black")
+          .attr("fill", "white")
     }
 }
+
+new FileDisplayPage()
