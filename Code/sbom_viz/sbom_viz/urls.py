99f4d08123c29235efdfa39420eedd23963fdea7
Camron Rule <cmrule@wm.edu>
2024-10-15 21:25:49 -0400

    Add ability to send Http request to a stored JSON file

--- Code/sbom_viz/sbom_viz/urls.py
+++ Code/sbom_viz/sbom_viz/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from sbom_viz import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name = "home")
+    path("", views.home, name = "home"),
+    re_path(r"[a-zA-Z]*.json$", views.json, name = "json") # allow D3 to query for data
 ]
