cdf582fefc23388eb6411961423912f24458af19
Manel Leong <mleong@email.wm.edu>
2024-10-20 15:35:23 -0400

    Added mock tree and tree/ endpoint

--- Code/sbom_viz/sbom_viz/views.py
+++ Code/sbom_viz/sbom_viz/views.py
@@ -1,5 +1,110 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+mock_tree = {
+    "name" : "SBOM Root", # artificial root node
+    "type" : "ROOT", # special type for root node, not official SBOM
+    "ghost" : False,
+    "relationships" : 
+    {
+        "RELATED_TO" : ["SPDXRef-DOCUMENT", "SPDXRef-CommonsLangSrc"] # RELATED_TO is a generic relationship not official SBOM
+    },
+    "children" : [
+        {
+            "name" : "SPDXRef-DOCUMENT",
+            "type" : "DOCUMENT",
+            "ghost" : False,
+            "relationships" :
+            {
+                "CONTAINS" : ["SPDXRef-Package"],
+                "COPY_OF" : ["DocumentRef-spdx-tool-1.2:SPDXRef-ToolsElement"],
+                "DESCRIBES" : ["SPDXRef-File", "SPDXRef-Package"]
+            },
+            "children" : [
+                {
+                    "name" : "SPDXRef-Package",
+                    "type" : "PACKAGE",
+                    "ghost" : False,
+                    "relationships" : 
+                    {
+                        "CONTAINS" : ["SPDXRef-JenaLib"],
+                        "DYNAMIC_LINK" : ["SPDXRef-Saxon"]
+                    },
+                    "children" : [
+                        {
+                            "name" : "SPDXRef-JenaLib",
+                            "type" : "FILE",
+                            "ghost" : False,
+                            "relationships" :
+                            {
+                                "CONTAINS" : ["SPDXRef-Package"]
+                            },
+                            "children" : [
+                                {
+                                    "name" : "SPDXRef-Package",
+                                    "type" : "PACKAGE",
+                                    "ghost" : True,  # for now, ghost nodes have no relationships or children, we can add in relationships if we think it's needed
+                                    "relationships" : {},
+                                    "children" : []
+                                }
+                            ]
+                        },
+                        {
+                            "name" : "SPDXRef-Saxon",
+                            "type" : "PACKAGE",
+                            "ghost" : False,
+                            "relationships" : {},
+                            "children" : []
+                        }
+                    ]
+                },
+                {
+                    "name" : "DocumentRef-spdx-tool-1.2:SPDXRef-ToolsElement",
+                    "type" : "COMPONENT", # generic type (for when the sbom doesn't specify a type), not official SBOM 
+                    "ghost" : False,
+                    "relationships" : {},
+                    "children" : []
+                },
+                {
+                    "name" : "SPDXRef-File",
+                    "type" : "FILE",
+                    "ghost" : False,
+                    "relationships" :
+                    {
+                        "GENERATED_FROM" : ["SPDXRef-fromDoap-0"]
+                    },
+                    "children" : [
+                        {
+                            "name" : "SPDXRef-fromDoap-0",
+                            "type" : "FILE",
+                            "ghost" : False,
+                            "relationships" : {},
+                            "children" : []
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "name" : "SPDXRef-CommonsLangSrc",
+            "type" : "FILE",
+            "ghost" : False,
+            "relationships" : 
+            {
+                "GENERATED_FROM" : ["NOASSERTION"] # NOASSERTION is a special case
+            },
+            "children" : [
+                {
+                    "name" : "NOASSERTION",
+                    "type" : "NOASSERTION",
+                    "ghost" : True, # setting all NOASSERTION as ghost nodes since they are not real components
+                    "relationships" : {},
+                    "children" : []
+                }
+            ]
+        }
+    ]
+}
 
 def home(request):
     if request.method == "POST" and len(request.FILES) == 1:
@@ -10,3 +115,7 @@ def home(request):
         return render(request, 'sbom_viz/display_file.html', {"file_contents": file_contents})
     else:
         return render(request, 'sbom_viz/index.html')
+    
+def get_tree(request):
+    if request.method == "GET":
+        return JsonResponse(mock_tree)
