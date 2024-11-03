8f72f87f6d9aefe7434cf09476184992648466c9
tgeby <tgeby@icloud.com>
2024-11-02 20:39:14 -0400

    Added helper functions for storing license information #5

--- Code/sbom_viz/sbom_viz/scripts/parse_files.py
+++ Code/sbom_viz/sbom_viz/scripts/parse_files.py
@@ -10,11 +10,28 @@ class SPDXParser():
         self.license_frequency_map = {} 
         self.id_license_map = {}
         self.purl_id_map = {} # To be used to identify which component is associated with security vulnerabilities
-        
+
+    def add_to_licenses_frequency_map(self, license_string):
+        if (license_string not in self.license_frequency_map):
+            self.license_frequency_map[license_string] = 1
+        else:
+            self.license_frequency_map[license_string] += 1
+
+    def add_to_license_map(self, id, license): 
+        # This method will return an integer value for how many times licenses were ignored with a new value
+        # This would happen if an id has a license added to it multiple times
+        ignore_count = 0
+        if (id in self.id_license_map and license != self.id_license_map[id]):
+            ignore_count += 1
+        elif (id not in self.id_license_map):
+            self.id_license_map[id] = license
+        return ignore_count
+
 
     def parse_file(self, path):
         with open(path, 'r') as f:
             self.data = f.read()
+        
     
     def get_document(self):
         return self.document
