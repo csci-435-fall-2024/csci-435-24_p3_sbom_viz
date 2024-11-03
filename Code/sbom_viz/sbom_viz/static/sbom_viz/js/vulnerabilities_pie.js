3b0b3b13bcc86c1de6ae1a1d92e1d619c6b7ef2e
camronrule <camronrule@gmail.com>
2024-11-02 21:59:58 -0400

    Make pie chart a little prettier (extra label), move label

--- Code/sbom_viz/sbom_viz/static/sbom_viz/js/vulnerabilities_pie.js
+++ Code/sbom_viz/sbom_viz/static/sbom_viz/js/vulnerabilities_pie.js
@@ -1,12 +1,17 @@
 import "https://d3js.org/d3.v7.min.js";
 
 // Temporary vulnerability distribution data (replace with endpoint later)
+// ** Just added a few more to see how pie chart looks when crowded
 let data = [
     {"name":"Critical", "count":10},
+    {"name":"Critical-High", "count":15},
     {"name":"High", "count":20},
+    {"name":"High-Medium", "count":25},
     {"name":"Medium", "count":10},
+    {"name":"Medium-Low", "count":15},
     {"name":"Low", "count":30},
-    {"name":"None", "count":90},
+    {"name":"Low-None", "count":35},
+    {"name":"None", "count":2},
 ]
 
 // Inspired by https://d3-graph-gallery.com/graph/pie_annotation.html
@@ -20,6 +25,7 @@ const svg = d3.select("#pie-chart-container")
                 .append("svg")
                     .attr("width", width)
                     .attr("height", height)
+                    .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;") // change if wanted, from the same link as the colors
                 .append("g")
                     .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
@@ -38,6 +44,14 @@ const color = d3.scaleOrdinal()
                 .domain(data.map(d => d.name))
                 .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length))
 
+const labelRadius = arc.outerRadius()() * 0.8;
+
+// A separate arc generator for labels 
+// Used to check if there is enough space for the label
+const arcLabel = d3.arc()
+    .innerRadius(labelRadius)
+    .outerRadius(labelRadius);
+
 // Draw the slices of the pie
 svg.selectAll("path")
     .data(pie(data))
@@ -46,14 +60,33 @@ svg.selectAll("path")
         .attr("d", arc)
         .attr("fill", d => color(d.data.name))
         .attr("stroke", "white")
-    .style("stroke-width", "2px");
+    .style("stroke-width", "2px")
+    // on hover, show the name of this category
+    // TODO -> implement a more through mouseover behavior
+    // like create a div that has:
+    //
+    // name
+    // count
+    // percentage of total vulnerabilities
+    .append("title")
+        .text(d => d.data.name); // show name on hover TODO make this more thorough
+    
 
-// Add labels to each slice
+// Add labels and count to each slice
 svg.selectAll("text")
     .data(pie(data))
     .enter()
     .append("text")
-        .attr("transform", d => `translate(${arc.centroid(d)})`)
-        .attr("dy", "0.35em")
+        // this is the bold label of the caregory
+        .attr("transform", d => `translate(${arcLabel.centroid(d)})`) // using arcLabel instead of arc moves it towards the border
         .attr("text-anchor", "middle")
-    .text(d => d.data.name);
+        .call(text => text.append("tspan")
+            .attr("y", "-0.4em")
+            .attr("font-weight", "bold")
+            .text(d => d.data.name))
+        // and this is the smaller label for the count of this category
+        .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan") // only show count if it will fit
+            .attr("x", 0)
+            .attr("y", "0.7em")
+            .attr("fill-opacity", 0.7)
+            .text(d => d.data.count));
