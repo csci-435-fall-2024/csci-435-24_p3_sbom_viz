cc2dd25b29b28a5b9eb14479635981ac18a1ab78
RachelZheng <rzhe2003@gmail.com>
2024-11-24 13:11:49 -0500

    fix key errors, change cveid to vulnerbaility id

--- Code/sbom_viz/sbom_viz/scripts/security.py
+++ Code/sbom_viz/sbom_viz/scripts/security.py
@@ -2,14 +2,43 @@ import subprocess
 import json
 import os
 
-def run_security_scan(path_to_sbom: str):
-    #To Do: sanitize input, error checks, bomber scan for backup
-    # runs from current directory
+def run_bomber_scan(path_to_sbom: str):
+    bomber_output=subprocess.run(args=["bomber", "scan", path_to_sbom, "--output", "json"], text=True)
+
+def run_trivy_scan(path_to_sbom:str):
     result=subprocess.run(args=["./sbom_viz/security_scanning_tools/executables/trivy_0.57.0_windows-64bit/trivy.exe", "sbom", "--format=json", path_to_sbom], text=True, capture_output=True)
-    #bomber_output=subprocess.run(args=["../security_scanning_tools/executables/bomber_0.5.1_windows_amd64/bomber.exe", "scan", path_to_sbom, "--output", "json"], text=True)
-    trivy_output=result.stdout.strip()
-    #print(bomber_output)
-    return json.loads(trivy_output)
+    #result=subprocess.run(args=["trivy", "sbom", "--format=json", path_to_sbom], text=True, capture_output=True)
+    
+    # if there is no stdout, it's likely that trivy wasn't able to scan the sbom due to errors like "file not found"
+    if result.stdout=='':
+        print(result.stderr)
+        return False
+    trivy_output=json.loads(result.stdout.strip())
+
+    # if trivy found no error (?) or was unable to scan for errors due to missing PURL
+    if "Results" not in trivy_output.keys():
+        return False
+    
+    return trivy_output
+
+def run_security_scan(path_to_sbom: str):
+    # To Do: sanitize input, error checks, bomber scan for backup, file type check for scanning tools
+    # Note: runs from current directory
+    scan_type="trivy"
+    # run trivy first
+    result=run_trivy_scan(path_to_sbom)
+
+    # if the trivy scan doesn't work, use bomber
+    if result==False:
+        scan_type="bomber"
+        result=run_bomber_scan(path_to_sbom)
+    
+    # if neither work, exit and return error message
+    if result==False:
+        print("unable to scan for security vulnerabilites")
+        exit()
+
+    return result
 
 def update_severity_distribution(sec_info:dict, vuln_dict:dict):
     severity=vuln_dict["Severity"]
@@ -42,7 +71,7 @@ def update_top_10(sec_info:dict, vuln_dict:dict, sbom_id, cve_to_remove=None):
                 top_10_vuln[key]=sbom_id
             else:
                 top_10_vuln[key]=vuln_dict[key]
-        sec_info["Summary"]["Top_10"][vuln_dict["CVE_ID"]]=top_10_vuln
+        sec_info["Summary"]["Top_10"][vuln_dict["VulnerabilityID"]]=top_10_vuln
             
 # directly parses sbom
 def find_corresponding_sbom_component_old(purl:str, sbom_file):
@@ -65,7 +94,8 @@ def find_corresponding_sbom_component(purl:str, sbom_parser):
 
 
 def make_vuln_dict(vuln_info):
-    vuln_dict={"CVE_ID": "",
+    keys_missing=[]
+    vuln_dict={"VulnerabilityID": "",
                 "SeveritySource":"",
                 "Status":"",
                 "Title":"",
@@ -73,17 +103,29 @@ def make_vuln_dict(vuln_info):
                 "Severity": "",
                 "CWE_IDs":[],
                 "CVSS": {},
-                "Displayed_CVSS":0,
+                "Displayed_CVSS":None,
                 "References": [],
                 "PublishedDate":"",
                 "LastModifiedDate":""}
     try:
+        # find the missing keys to prevent key errors from crashing the code
         for key in vuln_dict.keys():
-            if key in ["SeveritySource", "Status", "Title", "Description", "Severity", "CVSS", "PublishedDate", "LastModifiedDate"]:
+            if key in ["VulnerabilityID", "SeveritySource", "Status", "Title", "Description", "Severity", "CVSS", "PublishedDate", "LastModifiedDate"] and key not in vuln_info.keys():
+                keys_missing.append(key)
+                print(key, "missing")
+            elif key == "CWE_IDs" and "CweIDs" not in vuln_info.keys():
+                keys_missing.append(key)
+            elif key ==  "Displayed_CVSS" and "CVSS" in keys_missing:
+                keys_missing.append("Displayed_CVSS")
+            elif key == "References":
+                pass
+
+        for key in vuln_dict.keys():
+            if key in keys_missing:
+                continue
+            elif key in ["VulnerabilityID", "SeveritySource", "Status", "Title", "Description", "Severity", "CVSS", "PublishedDate", "LastModifiedDate"]:
                 vuln_dict[key]=vuln_info[key]
-            elif key == "CVE_ID":
-                vuln_dict[key]=vuln_info["VulnerabilityID"]
-            elif key == "CWE_IDs" and "CweIDs" in vuln_info.keys():
+            elif key == "CWE_IDs":
                 vuln_dict[key]=vuln_info["CweIDs"]
             elif key ==  "Displayed_CVSS":
                 vuln_dict[key]=vuln_info["CVSS"][vuln_dict["SeveritySource"]]["V3Score"]
@@ -127,14 +169,16 @@ def reformat_trivy_output(scan_output, sbom_parser, stop_ind=-1):
             # update summary
             update_severity_distribution(sec_info, vuln_dict)
             cvss=vuln_dict["Displayed_CVSS"]
-            if len(top_10_cvss)<10:
-                top_10_cvss[vuln_dict["CVE_ID"]]=cvss
+            if cvss==None:
+                pass
+            elif len(top_10_cvss)<10:
+                top_10_cvss[vuln_dict["VulnerabilityID"]]=cvss
                 top_10_cvss={key: value for key, value in sorted(top_10_cvss.items(), key=lambda item: item[1], reverse=True)}
                 update_top_10(sec_info, vuln_dict, sbom_id)
             elif cvss>list(top_10_cvss.items())[-1][1]: #tie?
                 lowest_cve, lowest_cvss = top_10_cvss.popitem()
                 cve_to_remove=lowest_cve
-                top_10_cvss[vuln_dict["CVE_ID"]]=cvss
+                top_10_cvss[vuln_dict["VulnerabilityID"]]=cvss
                 top_10_cvss={key: value for key, value in sorted(top_10_cvss.items(), key=lambda item: item[1], reverse=True)} 
                 update_top_10(sec_info, vuln_dict, sbom_id, cve_to_remove)
             
@@ -167,3 +211,7 @@ def get_security_output(sbom_parser):
     final_security_output=reformat_trivy_output(scan_output, sbom_parser)
     remove_sbom("sbom.json") # should I remove? #are we storing already loaded sboms
     return final_security_output
+
+# scan=run_trivy_scan("../../Artifacts/Examples/Working SBOMs/sbom2.3.spdx.json")
+# print(scan)
+
