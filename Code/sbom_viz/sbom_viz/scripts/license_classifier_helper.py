48f9c2aa4ecf435007842fb9fe1ab19cdbea6776
cmrule <cmrule@wm.edu>
2024-12-07 05:12:03 -0500

    Windows compatability

--- Code/sbom_viz/sbom_viz/scripts/license_classifier_helper.py
+++ Code/sbom_viz/sbom_viz/scripts/license_classifier_helper.py
@@ -1,12 +1,35 @@
 import subprocess
 import json
+import os
 
 def process_with_go_script(cleaned_licenses):
+
+    # assume this is the first time running
+    if not (os.path.isfile(os.path.join(os.getcwd(), "go.mod"))):
+
+        print("go.mod not found - installing license classifier library and init module...")
+
+        # init Go
+        subprocess.run(
+            ['go', 'mod', 'init', 'license-classifier'],
+            shell=True
+        )
+
+        # install module
+        subprocess.run(
+            ['go', 'get', 'github.com/google/licenseclassifier'],
+            shell=True
+        )
+
+        print("Finished!")
+
     try:
         cleaned_json = json.dumps(cleaned_licenses)
         # Run the Go script using subprocess
+        rel_path = "sbom_viz\\scripts\\license_classifier.go"
+        exec_dir = os.path.join(os.getcwd(), rel_path)
         result = subprocess.run(
-            ['./sbom_viz/scripts/license_classifier'],  # Command to run Go script
+            ['go', 'run', exec_dir],
             input=cleaned_json,  # Pass cleaned licenses as input
             text=True,  # Interpret input/output as text (not bytes)
             capture_output=True,  # Capture stdout and stderr
