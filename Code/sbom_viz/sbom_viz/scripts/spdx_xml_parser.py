00350de9d5c0f5145da52b7fdd3a0c2104fcd6dd
Manel Leong <mleong@email.wm.edu>
2024-12-18 03:10:21 -0500

    Added xml parser and removed print line from map builder

--- Code/sbom_viz/sbom_viz/scripts/spdx_xml_parser.py
+++ Code/sbom_viz/sbom_viz/scripts/spdx_xml_parser.py
@@ -10,6 +10,7 @@ class SpdxXmlParser():
         self.sbom_dict = {}
         self.data = ""
         self.version = ""
+        self.root_key = ""  # Dynamically detected root key
         self.components_list = []
         self.relationship_list = []
         self.id_data_map = {}
@@ -17,12 +18,16 @@ class SpdxXmlParser():
         self.id_license_map = {}
         self.purl_id_map = {}
 
+    def detect_root_key(self):
+        """Find the root key dynamically from the parsed XML."""
+        self.root_key = next(iter(self.sbom_dict))
+
     def add_to_licenses_frequency_map(self, license_string):
         """
         This function is to be called from parse_licensing_information. It handles the logic for either adding a new license to the 
         frequency map or increment the license's corresponding value if it is already present.
         """
-        if (license_string not in self.license_frequency_map):
+        if license_string not in self.license_frequency_map:
             self.license_frequency_map[license_string] = 1
         else:
             self.license_frequency_map[license_string] += 1
@@ -33,25 +38,30 @@ class SpdxXmlParser():
         processes components with multiple license definitions. As of now, duplicates are ignored.
         """
         ignore_count = 0
-        if (id in self.id_license_map and license != self.id_license_map[id]): # True when an id already has a license stored that is different from the passed in license
-            ignore_count += 1 
-        elif (id not in self.id_license_map): 
+        if id in self.id_license_map and license != self.id_license_map[id]:
+            ignore_count += 1
+        elif id not in self.id_license_map: 
             self.id_license_map[id] = license
         return ignore_count
 
     def parse_licensing_information(self):
-        """
-        This function fills self.license_frequency_map and self.id_data_map with sbom component data.
-        It is only intended to be called by self.parse_file().
-        """
-        pass
+        try:
+            files = self.sbom_dict.get(self.root_key, {}).get('files', [])
+            packages = self.sbom_dict.get(self.root_key, {}).get('packages', [])
+            for component in files + packages:
+                license_string = component.get('licenseDeclared', 'NOASSERTION')
+                self.add_to_licenses_frequency_map(license_string)
+                spdx_id = component.get('SPDXID', 'Unknown')
+                self.add_to_id_license_map(spdx_id, license_string)
+        except Exception as e:
+            print(f"Error parsing licensing information: {e}")
 
     def find_version(self):
         """
         Determine the version of SPDX and store it as a string in self.version.
         It is only intended to be called by self.parse_file().
         """
-        pass
+        self.version = self.sbom_dict.get(self.root_key, {}).get('spdxVersion', 'Unknown')
 
     def parse_document_information(self):
         """
@@ -59,22 +69,76 @@ class SpdxXmlParser():
         self.components_list.
         It is only intended to be called by self.parse_file().
         """
-        pass
+        document = self.sbom_dict.get(self.root_key, {})
+        metadata = {
+            'id': document.get('SPDXID', 'Unknown'),
+            'name': document.get('name', 'Unknown'),
+            'dataLicense': document.get('dataLicense', 'Unknown'),
+            'creationInfo': document.get('creationInfo', {})
+        }
+        self.components_list.append(metadata)
 
     def parse_component_information(self):
         """
         This function adds the remainder of the components to self.components_list. 
         It is only intended to be called by self.parse_file(), and after self.parse_document_information().
         """
-        pass
+        try:
+            document = self.sbom_dict.get(self.root_key, {})
+            files = document.get('files', [])
+            if isinstance(files, dict):
+                files = [files]
+            elif not isinstance(files, list):
+                files = []
+
+            for file in files:
+                if isinstance(file, dict):
+                    self.components_list.append({
+                        'id': file.get('SPDXID', "Unknown"),
+                        'name': file.get('fileName', "Unknown"),
+                        'licenseConcluded': file.get('licenseConcluded', "NOASSERTION")
+                    })
+
+            packages = document.get('packages', [])
+            if isinstance(packages, dict):
+                packages = [packages]
+            elif not isinstance(packages, list):
+                packages = []
+
+            for package in packages:
+                if isinstance(package, dict):
+                    self.components_list.append({
+                        'id': package.get('SPDXID', "Unknown"),
+                        'name': package.get('name', "Unknown"),
+                        'licenseDeclared': package.get('licenseDeclared', "NOASSERTION")
+                    })
+
+        except Exception as e:
+            print(f"Error parsing components: {e}")
 
     def parse_relationship_information(self):
         """
-        This function fills self.relationship_list
+        Extract relationships and populate self.relationship_list.
         """
-        relationship_list = []
-        self.relationship_list = relationship_list
+        try:
+            relationships = self.sbom_dict.get(self.root_key, {}).get('relationships', [])
 
+            if isinstance(relationships, dict):
+                relationships = [relationships]
+            elif not isinstance(relationships, list):
+                relationships = []
+
+            for relationship in relationships:
+                if isinstance(relationship, dict):
+                    self.relationship_list.append({
+                        'source_id': relationship.get('spdxElementId', "Unknown"),
+                        'target_id': relationship.get('relatedSpdxElement', "Unknown"),
+                        'type': relationship.get('relationshipType', "Unknown")
+                    })
+                else:
+                    print(f"Skipping invalid relationship entry: {relationship}")
+        except Exception as e:
+            print(f"Error parsing relationships: {e}")
 
     def parse_id_to_data_map(self):
         """
@@ -83,7 +147,8 @@ class SpdxXmlParser():
         This function is only intended to be called from parse_file(). 
         get_id_data_map() should be used to acquire the dictionary object after a file is parsed. 
         """
-        pass
+        for component in self.components_list:
+            self.id_data_map[component['id']] = component
     
     def parse_purl_to_id_map(self):
         """
@@ -91,11 +156,19 @@ class SpdxXmlParser():
         with.
         This function is only intended to be called from parse_file(). It needs to be called after parse_components().
         """
-        pass
+        for package in self.sbom_dict.get(self.root_key, {}).get('packages', []):
+            spdx_id = package.get('SPDXID', 'Unknown')
+            external_refs = package.get('externalRefs', [])
+            if isinstance(external_refs, dict):
+                external_refs = [external_refs]
+            for ref in external_refs:
+                if 'purl' in ref.get('referenceType', '').lower():
+                    self.purl_id_map[ref.get('referenceLocator', '')] = spdx_id
 
     def parse_file(self, file_string):
         self.sbom_dict = xmltodict.parse(file_string)
         self.data = file_string
+        self.detect_root_key()
         self.find_version()
         self.parse_licensing_information()
         self.parse_document_information()
@@ -115,10 +188,13 @@ class SpdxXmlParser():
     
     def get_license_information(self):
         """Return license information in a format requested by the front-end team."""
-        license_info = {}
-        license_info['distribution'] = self.license_frequency_map
-        license_info['mapping'] = self.id_license_map
-        return license_info
+        return {
+            'distribution': self.license_frequency_map,
+            'mapping': self.id_license_map
+        }
     
     def get_purl_id_map(self):
         return self.purl_id_map
+    
+    def get_sbom_data(self):
+        return ("xml", self.data)
